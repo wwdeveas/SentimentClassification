ebe91003f49b7b6ffb35c1ec4da86f276d653c25
wwdeveas <112140370+wwdeveas@users.noreply.github.com>
2023-05-01 12:44:46 -0500

    Update vectorizer.py

--- SentimentClassification/movieclassifier/vectorizer.py
+++ SentimentClassification/movieclassifier/vectorizer.py
@@ -3,10 +3,12 @@ import re
 import os
 import pickle
 
+#joins the pkl objects and the stop words in the same directory
 cur_dir = os.path.dirname(__file__)
 stop = pickle.load(open(
     os.path.join(cur_dir, 'pkl_objects', 'stopwords.pkl'), 'rb'))
 
+#function that executes the tokenizer
 def tokenizer(text):
     text = re.sub('<[^>]*>', '', text)
     emoticons = re.findall('(?::|;|=) (?:-) ? (?:\) | \(|D|P)',
